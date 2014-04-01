d854cf45b1d8193b577b49dbbbbee4757d774c4f
sunflower <sunflower@sunflowers-iMac.local>
2014-04-01 00:40:15 -0500

    New solution for paranthesis match problem

---
+++ java_solutions/src/solutions/ParenthesisMatchProblem.java
@@ -0,0 +1,63 @@
+package solutions;
+
+import utils.MyGenericStack;
+
+public class ParenthesisMatchProblem {
+	public String inputString;
+	
+	/**
+	 * Stack to save the indices of the braces as the input string is traversed to determine validity
+	 */
+	private MyGenericStack<Integer> indexStack; 
+	public int maxDistanceBetweenBraces;
+	
+	public ParenthesisMatchProblem (String input)
+	{
+		this.inputString = input;
+		indexStack = new MyGenericStack<Integer>();
+	}
+	
+	/**
+	 * Check if braces match. Also find the maximum distance 
+	 * @return
+	 * @throws Exception
+	 */
+	public Boolean Run () throws Exception
+	{
+		int stringIndex = 0;
+		maxDistanceBetweenBraces = 0;
+		while (stringIndex < inputString.length())
+		{
+			char c = inputString.charAt(stringIndex);
+			switch (c)
+			{
+				case '{':
+					indexStack.push(stringIndex);
+					break;
+				case '}':
+					Integer lastIndex = indexStack.pop();
+					if (lastIndex == null)
+					{
+						// Failed to pop from stack, the string is not matched
+						maxDistanceBetweenBraces = 0;
+						return false;
+					}
+					int distanceBetweenBraces = stringIndex - lastIndex;
+					if (distanceBetweenBraces > maxDistanceBetweenBraces)
+					{
+						maxDistanceBetweenBraces = distanceBetweenBraces;
+					}
+					break;
+				default:
+					throw new Exception("Unknown character in the string");
+			}
+			stringIndex++;
+		}
+		if (indexStack.Empty())
+		{
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ java_solutions/src/utils/MyGenericStack.java
@@ -0,0 +1,72 @@
+package utils;
+
+import java.util.ArrayList;
+public class MyGenericStack<T> {
+	ArrayList<T> myList;
+	
+	public MyGenericStack()
+	{
+		myList = new ArrayList<T>();
+	}
+	
+	/**
+	 * Get the size of the list
+	 * @return Stack length
+	 */
+	public int Length()
+	{
+		return myList.size();
+	}
+	
+	public Boolean Empty()
+	{
+		return (this.Length() == 0);
+	}
+	
+	/**
+	 * Push or insert element at the end of the stack
+	 * @param element
+	 */
+	public void push(T element)
+	{
+		try
+		{
+			System.out.println("Insert " + element.toString() + " to stack");
+			myList.add(element);
+		}
+		catch (Exception ex)
+		{
+			System.out.printf("Exception when inserting into stack: " + ex.getMessage());
+		}
+	}
+	
+	/**
+	 * Get the last element in the stack
+	 * @return last element
+	 */
+	public T pop()
+	{
+		try
+		{
+			T element = myList.remove(this.Length() - 1);
+			System.out.println("Remove element " + element.toString() + " from stack");
+			return element;
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Exception when removing from stack: " + ex.getMessage());
+		}
+		return null;
+	}
+	
+	/**
+	 * Display all elements in stack
+	 */
+	public void Display()
+	{
+		for (T element: myList)
+		{
+			System.out.println(element);
+		}
+	}
+}
